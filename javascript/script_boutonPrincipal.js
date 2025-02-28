5172d20831a3e91d0ed3fc7ddb918fe60c2b5b11
Julie <xxjulie.leroyxx@gmail.com>
2025-02-28 13:44:09 +0100

    modification

--- javascript/script_boutonPrincipal.js
+++ javascript/script_boutonPrincipal.js
@@ -6,15 +6,30 @@ function affiche() {
     bouton.style.display = "none";
     div_affichage.style.backgroundImage =  "url('images/julie.png')";
     div_affichage.style.paddingTop = "30%"
-    let presentation1 = document.createElement("p");
-    presentation1.setAttribute("id", "paragraphe1")
-    presentation1.appendChild(document.createTextNode("Bonjour, je m'appelle Julie Leroy \n Je vous souhaite la bienvenue sur mon Eporfolio "));
+    let presentation = document.createElement("ul");
+    let elm1 = document.createElement("li")
+    let elm2 = document.createElement("li")
+    let elm3 = document.createElement("li")
+    let elm4 = document.createElement("li");
+    let elm5 = document.createElement("li");
+    let elm6 = document.createElement("li");
+    elm1.appendChild(document.createTextNode("Bonjour, je m'appelle Julie Leroy, je vous souhaite la bienvenue sur mon Eporfolio "));
+    elm2.appendChild(document.createTextNode("Voici mes informations accésible rapidement: "));
+    elm3.appendChild(document.createTextNode("Adresse email : julie.leroy02pr@hotmail.com "));
+    elm4.appendChild(document.createTextNode("Numéro de téléphone : Me contacter par email d'abord"))
+    elm5.appendChild(document.createTextNode("Date de naissance : 5 février 2005 (20 ans)"))
+    elm6.appendChild(document.createTextNode("Lien LinkedIn : https://www.linkedin.com/in/julie-leroy-82872b2aa/"))
+    presentation.style.listStyle = "none"
+    presentation.style.textAlign = "center"
 
-    presentation1.style.textAlign = "center"
-
-    presentation1.style.backgroundColor = "#a4ced5"
-
-    div_affichage.appendChild(presentation1);
+    presentation.style.backgroundColor = "#a4ced5"
+    presentation.appendChild(elm1)
+    presentation.appendChild(elm2)
+    presentation.appendChild(elm5)
+    presentation.appendChild(elm3)
+    presentation.appendChild(elm4)
+    presentation.appendChild(elm6)
+    div_affichage.appendChild(presentation);
 
 }
 
