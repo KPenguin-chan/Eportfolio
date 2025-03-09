88d92b69355c9388d788304231970c33f6d88dd5
Julie <xxjulie.leroyxx@gmail.com>
2025-03-09 18:45:38 +0100

    ajout nouvelles fonctionnalités

--- javascript/script_formation.js
+++ javascript/script_formation.js
@@ -1,7 +1,7 @@
 window.onload = init;
 
 function vers_gauche() {
-    console.log("gauche")
+
     let interrieur = document.getElementById("carrousel_int");
     let exterrieur = document.getElementById("carrousel_ext");
 
@@ -15,9 +15,27 @@ function vers_gauche() {
         interrieur.appendChild(suivant);
     }
 }
+
+function vers_droite() {
+
+    let interrieur = document.getElementById("carrousel_int");
+    let exterrieur = document.getElementById("carrousel_ext");
+
+    let activ_elmt = interrieur.firstElementChild;
+    let precedent = exterrieur.lastChild;
+
+    if (activ_elmt && precedent) {
+        interrieur.removeChild(activ_elmt);
+        exterrieur.appendChild(activ_elmt);
+        exterrieur.removeChild(precedent);
+        interrieur.appendChild(precedent);
+    }
+}
 function init() {
 
     let bGauche = document.getElementById("bGauche");
+    let bDroite = document.getElementById("bDroite");
 
+    bDroite.onclick = vers_droite;
     bGauche.onclick = vers_gauche;
 }
